6222c070a5cc0435d703fd23371a0124b6de512c
LilArash <144728191+LilArash@users.noreply.github.com>
2024-02-17 10:22:50 -0800

    Create script.js

---
+++ assets/script/script.js
@@ -0,0 +1,14 @@
+let menu = document.querySelector( ".menu")
+let menuBtn = document.querySelector( ".header__icon")
+let menuBtnIcon = document.querySelector(".header__icon i")
+menuBtn.addEventListener("click", function() {
+    if(menuBtnIcon.classList.contains("fa-bars")) {
+        menu.style.left = "0"
+        menuBtnIcon.classList = "fa fa-times"
+    }
+    else 
+    {
+        menu.style.left = "-16rem"
+        menuBtnIcon.classList = "fa fa-bars"
+    }
+})
