ffeb02f867624a8bf427cc95869f240a08086d2f
LilArash <144728191+LilArash@users.noreply.github.com>
2024-02-22 04:17:47 -0800

    JavaScript file

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,14 +1,28 @@
-let menu = document.querySelector( ".menu")
-let menuBtn = document.querySelector( ".header__icon")
-let menuBtnIcon = document.querySelector(".header__icon i")
-menuBtn.addEventListener("click", function() {
-    if(menuBtnIcon.classList.contains("fa-bars")) {
+const menu = document.querySelector(".menu")
+const menuBtn = document.querySelector(".header__icon")
+const menuBtnIcon = document.querySelector(".header__icon i")
+menuBtn.addEventListener("click", () => {
+    if (menuBtnIcon.classList.contains("fa-bars")) {
         menu.style.left = "0"
         menuBtnIcon.classList = "fa fa-times"
     }
-    else 
-    {
+    else {
         menu.style.left = "-16rem"
         menuBtnIcon.classList = "fa fa-bars"
     }
 })
+function loaderFunction() {
+    const loader = document.querySelector('.loader');
+    const loadingIcon = document.querySelector('.loader i');
+    setTimeout(() => {
+        loader.style.right = "0";
+        loadingIcon.style.color = "#87BC25";
+    }, 1000)
+    window.addEventListener('DOMContentLoaded', () => {
+        //setTimeout(() => {
+            loader.style.left = "100%";
+            loader.firstElementChild.remove();
+        //}, 3000);
+    });
+}
+loaderFunction();
